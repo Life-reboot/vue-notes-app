00cb7dd6af1c76b881f5cfea39e83a3a43d763fa
Peter Jang <peter.x.jang@gmail.com>
2020-04-21 19:35:06 -0500

    Break everything into sub-components

---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="note-selectors">
+    <note-selector />
+    <note-selector />
+    <note-selector />
+    <note-selector />
+    <note-selector />
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector";
+
+export default {
+  name: "note-selectors",
+  components: {
+    NoteSelector,
+  },
+};
+</script>
