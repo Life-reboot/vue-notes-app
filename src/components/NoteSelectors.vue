073ac82595845a0d89bc687d008a868b72c6aef7
Peter Jang <peter.x.jang@gmail.com>
2020-04-21 20:02:27 -0500

    Use data to display note selector components with v-for

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="note-selectors">
-    <note-selector />
-    <note-selector />
-    <note-selector />
-    <note-selector />
-    <note-selector />
+    <note-selector v-for="note in notes" v-bind:note="note" v-bind:key="note.id" />
   </div>
 </template>
 
@@ -15,6 +11,16 @@ export default {
   name: "note-selectors",
   components: {
     NoteSelector,
+  },
+  data: function() {
+    return {
+      notes: [
+        { id: 1, body: "This is the first note", timestamp: Date.now() },
+        { id: 2, body: "This is the second note", timestamp: Date.now() },
+        { id: 3, body: "This is the third note", timestamp: Date.now() },
+        { id: 4, body: "This is the fourth note", timestamp: Date.now() },
+      ],
+    };
   },
 };
 </script>
